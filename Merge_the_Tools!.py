8e5803183cb5fde9ec5d2594325510772f452a4c
AKURATHI SASIDHAR <22kt1a0595@gmail.com>
2024-08-16 06:46:25 +0530

    Solved "Merge the Tools!" Challenge

---
+++ Merge_the_Tools!.py
@@ -0,0 +1,24 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    i = 0
+    p = k
+    t = []
+    while i < len(string):
+        t.append(string[i:k])
+        i = k
+        k = k + p
+    temp = ""
+    
+    for j in t:
+        for x in j:
+            if x in temp:
+                continue
+            else:
+                temp = temp + x
+        print(temp)
+        temp = ""
+             
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
